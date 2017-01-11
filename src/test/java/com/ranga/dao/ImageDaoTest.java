1f5697e3c72529d5306465792bd1bc40e8e022a5
DmitrySay <sdv1sdv1@mail.ru>
2017-01-11 14:13:49 +0300

    EGX-1. add Test

---
+++ src/test/java/com/ranga/dao/ImageDaoTest.java
@@ -0,0 +1,35 @@
+package com.ranga.dao;
+
+
+import com.ranga.entities.Image;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@ContextConfiguration("/applicationTestConfig.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@TransactionConfiguration(defaultRollback = true)
+@Transactional
+public class ImageDaoTest {
+
+    @Autowired
+    private ImageDao imageDao;
+
+    @Test
+    public void getAllImagesTest() {
+
+        List<Image> list = imageDao.getAllImage();
+        Assert.assertNotNull(list);
+        Assert.assertTrue(list.size() > 0);
+
+    }
+
+
+}
